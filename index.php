89259298309ad8ad129ad1ef28a4c768185427ce
pasinJ <33993773+pasinJ@users.noreply.github.com>
2018-05-10 22:41:42 +0700

    6

--- index.php
+++ index.php
@@ -26,8 +26,12 @@ if (!$link) {
 
 echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
 echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
+echo "connectdbhost: " . $connectstr_dbhost;
+echo "connectdbname: " . $connectstr_dbname;
+echo "connectdbusername: " . $connectstr_dbusername;
+echo "connectdbpw: " . $connectstr_dbpassword;
 
-if (mysqli_query($links, '
+/*if (mysqli_query($links, '
 CREATE TABLE Products (
 `Id` INT NOT NULL AUTO_INCREMENT ,
 `ProductName` VARCHAR(200) NOT NULL ,
@@ -37,7 +41,7 @@ PRIMARY KEY (`Id`)
 );
 ')) {
 printf("Table created\n");
-}
+}*/
 
 mysqli_close($link);
 ?>
