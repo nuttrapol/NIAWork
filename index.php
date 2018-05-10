83db0de861516632df789f6aa369e1c50888e80d
pasinJ <33993773+pasinJ@users.noreply.github.com>
2018-05-10 22:58:20 +0700

    12

--- index.php
+++ index.php
@@ -1,57 +1,20 @@
 <?php
-$connectstr_dbhost = '';
-$connectstr_dbname = '';
-$connectstr_dbusername = '';
-$connectstr_dbpassword = '';
+session_start();
+require_once('connect.php');
 
-foreach ($_SERVER as $key => $value) {
-    if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
-        continue;
-    }
-    
-    $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
-    $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
-    $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
-    $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
-}
-
-$link = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword,$connectstr_dbname);
-
-if (!$link) {
-    echo "Error: Unable to connect to MySQL." . PHP_EOL;
-    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-    exit;
-}
 echo "connectdbhost: " . $connectstr_dbhost. "\n";
 echo "connectdbname: " . $connectstr_dbname. "\n";
 echo "connectdbusername: " . $connectstr_dbusername. "\n";
 echo "connectdbpw: " . $connectstr_dbpassword. "\n";
-/*echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
-echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;*/
 
-
-/*if (mysqli_query($links, '
-CREATE TABLE Products (
-`Id` INT NOT NULL AUTO_INCREMENT ,
-`ProductName` VARCHAR(200) NOT NULL ,
-`Color` VARCHAR(50) NOT NULL ,
-`Price` DOUBLE NOT NULL ,
-PRIMARY KEY (`Id`)
-);
-')) {
-echo "Table created\n";
-}else{
-echo "Error\n";
-}*/
 $q = "CREATE TABLE Products (`Id` INT NOT NULL AUTO_INCREMENT ,`ProductName` VARCHAR(200) NOT NULL ,`Color` VARCHAR(50) NOT NULL ,
 `Price` DOUBLE NOT NULL ,PRIMARY KEY (`Id`));";
 $result=$link->query($q);                    
                         if(!$result){
                             echo "Select failed. Error: ".$link->error ;
+                        }else{
+                            echo "OK " ;
                         }
-
-mysqli_close($link);
 ?>
 <html>
 <body>
