e2d56722dbfcea8b68cfb14a5fd40f1249a17aca
Sirichai Khomleart <sirichai@Sirichais-MacBook-Pro.local>
2018-05-15 22:51:55 +0700

    Multi keys and download text file
    -Add download text file for each topic
    -Multi keys query supported
    PS. Be aware of PERMISSION

--- poom/result.php
+++ poom/result.php
@@ -21,6 +21,15 @@
         $query = $_POST['q'];
     }
 
+    function extractString($string, $start, $end)
+    {
+        $string = " " . $string;
+        $ini = strpos($string, $start);
+        if ($ini == 0) return "";
+        $ini += strlen($start);
+        $len = strpos($string, $end, $ini) - $ini;
+        return substr($string, $ini, $len);
+    }
 
     function checkTypeQ($string)
     {
@@ -37,6 +46,13 @@
     function searchQ($curQ)
     {
         global $link;
+        $mydirectory = '/tempTextFiles/'. date("Y-m-d") .'/'. date("h:i:sa");
+        //$mydirectory = '';
+        if (!file_exists($mydirectory)) {
+            mkdir($mydirectory, 0777, true);
+        }
+
+
         $key = extractString($curQ, "{key=", ",");
         $key_array = explode("&", $key);
         $sdate = extractString($curQ, "sdate=", ",");
@@ -57,26 +73,79 @@
         echo "---> Room is: " . $room . "<br>";
 
         $topicRet_array = [];
+
+        function multiTitle($key_array)
+        {
+            //echo "test0".$key_array[0];
+            $strTitle = " ";
+            for ($k = 2; $k <= count($key_array); $k++) {
+                //echo "test".$key_array[$k-1];
+                $strTitle .= " OR (title like '%" . $key_array[$k - 1] . "%') ";
+            }
+            return $strTitle;
+        }
+
+        $mTitle = multiTitle($key_array);
+        //echo "here is my mistake".$mTitle;
+
+        function multiDesc($key_array)
+        {
+            //echo "test0".$key_array[0];
+            $strDesc = " ";
+            for ($k = 2; $k <= count($key_array); $k++) {
+                //echo "test".$key_array[$k-1];
+                $strDesc .= " OR (description like '%" . $key_array[$k - 1] . "%') ";
+            }
+            return $strDesc;
+        }
+
+        $mDesc = multiDesc($key_array);
+
+        function multiTag($key_array)
+        {
+            //echo "test0".$key_array[0];
+            $strTag = " ";
+            for ($k = 2; $k <= count($key_array); $k++) {
+                //echo "test".$key_array[$k-1];
+                $strTag .= " OR (tag like '%" . $key_array[$k - 1] . "%') ";
+            }
+            return $strTag;
+        }
+
+        $mTag = multiTag($key_array);
+
+
         $topicQ = "SELECT tid,title,description,created_time 
-                  FROM topic 
+                   FROM topic 
                   
-                  WHERE ((title like '%" . $key_array[0] . "%') 
-                  or (description like '%" . $key_array[0] . "%') 
-                  or (tag like '%" . $key_array[0] . "%')) 
+                   WHERE ((title like '%" . $key_array[0] . "%')" . $mTitle . " 
+                   or (description like '%" . $key_array[0] . "%') " . $mDesc . "
+                   or (tag like '%" . $key_array[0] . "%')) " . $mTag . "
                   
-                  and (created_time>='" . $sdate . " 00:00:00' 
-                  and created_time<='" . $edate . " 23:59:59')";
+                   and (created_time>='" . $sdate . " 00:00:00' 
+                   and created_time<='" . $edate . " 23:59:59')
+                   ORDER BY created_time ASC";
+
         $result = $link->query($topicQ);
         //echo "Database Topic Query: " . $topicQ . "<br>";
         echo "---> Number of topics: " . $result->num_rows . "<br><br>";
         echo "-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-<br><br>";
         while ($array = $result->fetch_assoc()) {
-            echo "Topic :" . $array['tid'] . "<br>Create At : " . $array['created_time'] . "<br>Title : " . $array['title'] . "<br>";
+            echo "TopicID :" . $array['tid'] . "<br>Create At : " . $array['created_time'] . "<br>Title : " . $array['title'] . "<br>";
             array_push($topicRet_array, $array['tid']);
-            $commentQ =  "SELECT cid,title,description,created_time,comment_no 
+
+            //add topic data to text file
+            //$qfile = fopen($mydirectory . "myText.txt", "w");
+            $qfile = fopen($mydirectory . "tid".$array['tid'].".txt", "w");
+            $txt = "<topic>\n<tid>" . $array['tid'] . "</tid>\n<title>" . $array['title'] . "</title>\n<createdTime>" . $array['created_time'] . "</createdTime>\n<description>" . $array['description'] . "</description>\n";
+            fwrite($qfile, $txt);
+
+
+            $commentQ = "SELECT cid,title,description,created_time,comment_no 
                           FROM comment 
                   
                           WHERE tid ='" . $array['tid'] . "'
+                          AND status='normal'
                         
                           order by comment_no asc";
             $resultComment = $link->query($commentQ);
@@ -84,7 +153,13 @@
             echo "Number of comments: " . $resultComment->num_rows . "<br><br>";
             while ($array = $resultComment->fetch_assoc()) {
                 echo "--------> CommentID :" . $array['cid'] . "<br>--------> Title : " . $array['title'] . "<br>";
-                $replyQ =  "SELECT rid,status,description,created_time 
+
+                //add comment data to text file
+                $txt = "<comment>\n<cid>" . $array['cid'] . "</cid>\n<title>" . $array['title'] . "</title>\n<createdTime>" . $array['created_time'] . "</createdTime>\n<description>" . $array['description'] . "</description>\n";
+                fwrite($qfile, $txt);
+
+
+                $replyQ = "SELECT rid,status,description,created_time 
                           FROM reply
                   
                           WHERE cid ='" . $array['cid'] . "'
@@ -96,9 +171,20 @@
                 echo "-------->Number of replies: " . $resultReply->num_rows . "<br>";
                 while ($array = $resultReply->fetch_assoc()) {
                     echo "----------------> ReplyID :" . $array['rid'] . "<br>";
+
+                    //add comment data to text file
+                    $txt = "<reply>\n<rid>" . $array['rid'] . "</rid>\n<createdTime>" . $array['created_time'] . "</createdTime>\n<description>" . $array['description'] . "</description>\n</reply>\n";
+                    fwrite($qfile, $txt);
                 }
+                $txt = "</comment>\n";
+                fwrite($qfile, $txt);
                 echo "<br><br>";
             }
+            $txt = "</topic>\n";
+            fwrite($qfile, $txt);
+            fclose($qfile);
+            echo "<a href=\"../html-link.htm\"><img src=\"images\doc_icon.png\" style=\"width:20px; height:20px\" title=\"Download Text File\" alt=\"Download\"></a>";
+            echo "<a href='myText.txt'><button type=\"button\">download txt file here</button></a><br><br>";
             //echo "FFF: ".$topicRet_array[0];
             echo "<br>-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-^-<br><br>";
         }
@@ -119,6 +205,23 @@
 
 </header>
 <body>
+<?php
+echo getcwd();
+
+//$content = "some text here";
+//$fp = fopen( "myText.txt","w");
+//fwrite($fp,$content);
+//fclose($fp);
+//
+//
+//$myfile = fopen("myText.txt", "w");
+//$txt = "John Doe\n";
+//fwrite($myfile, $txt);
+//$txt = "Jane Doe\n";
+//fwrite($myfile, $txt);
+//fclose($myfile);
+?>
+
 <a href="clearSession.php">Clear Session</a>
 <a href="search.php">Back to Search</a>
 
@@ -132,20 +235,10 @@
     Your queries:<br>
     <?php
 
-    echo $query . "<br><hr><br>";
+    echo $query . "<br>";
     //split lines
     $query_array = explode("\n", $query);
-
-
-    function extractString($string, $start, $end)
-    {
-        $string = " " . $string;
-        $ini = strpos($string, $start);
-        if ($ini == 0) return "";
-        $ini += strlen($start);
-        $len = strpos($string, $end, $ini) - $ini;
-        return substr($string, $ini, $len);
-    }
+    echo "<br>Total number of quries is " . count($query) . ".<br><br><hr><br>";
 
 
     for ($line = 1; $line <= count($query_array); $line++) {
