c25fb2801e9ec6e8178641e9e6e0e3db96860610
pasinJ <33993773+pasinJ@users.noreply.github.com>
2018-05-16 00:12:21 +0700

    azure

--- index1.php
+++ index1.php
@@ -1,34 +1,6 @@
 <?php
 session_start();
-$connectstr_dbhost = '';
-$connectstr_dbname = '';
-$connectstr_dbusername = '';
-$connectstr_dbpassword = '';
-$rootdir = $_SERVER['DOCUMENT_ROOT'];
-foreach ($_SERVER as $key => $value) {
-    if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
-        continue;
-    }
-    
-    $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
-    $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
-    $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
-    $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
-}
-
-$link = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword,$connectstr_dbname);
-/*$link = mysqli_connect("127.0.0.1:53388","azure", "6#vWHD_$", "localdb");*/
-
-echo $connectstr_dbhost . "<br>";
-echo $connectstr_dbusername . "<br>";
-echo $connectstr_dbpassword . "<br>";
-echo $connectstr_dbname . "<br>";
-if (!$link) {
-    echo "Error: Unable to connect to MySQL." . PHP_EOL;
-    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-    exit;
-}
+require_once('connect.php');
 date_default_timezone_set('Asia/Bangkok');
 
 /*echo "connectdbhost: " . $connectstr_dbhost. "\n";
